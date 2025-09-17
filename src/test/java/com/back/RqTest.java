a0f1f3fd365112af69ea656246501df164e75c9c
junseokPP <pjs981130@gmail.com>
2025-09-18 00:15:58 +0900

    11강 : rq.getParam(t7) - red

--- src/test/java/com/back/RqTest.java
+++ src/test/java/com/back/RqTest.java
@@ -66,4 +66,14 @@ public class RqTest {
         String actionName = rq.getParam("고향",""); // 삭제
         assertThat(actionName).isEqualTo("서울");
     }
+
+    @Test
+    @DisplayName("입력값 : \"등록?고향=서울&이름=홍길동\" : getParam(\"이름\"): 홍길동")
+    public void t7() {
+
+        Rq rq = new Rq("등록?고향=서울&이름=홍길동");
+
+        String actionName = rq.getParam("이름",""); // 삭제
+        assertThat(actionName).isEqualTo("홍길동");
+    }
 }
