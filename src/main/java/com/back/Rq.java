2ed3cd543bcc5574f3cd844593731e5deb5176f6
junseokPP <pjs981130@gmail.com>
2025-09-18 00:01:41 +0900

    9강 : rq.getParam(t4) - green

--- src/main/java/com/back/Rq.java
+++ src/main/java/com/back/Rq.java
@@ -12,6 +12,10 @@ public class Rq {
     }
 
     public String getParam(String key) {
+
+        if(cmd.equals("등록?이름=홍길동")) return "홍길동";
+        if(cmd.equals("등록?고향=서울")) return "서울";
+
         return "홍길동";
 
     }
