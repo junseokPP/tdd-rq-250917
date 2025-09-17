82b8a43f687cd8ff392181f9f04bda3ab582506f
junseokPP <pjs981130@gmail.com>
2025-09-18 00:16:54 +0900

    11강 : rq.getParam(t6,t7) - blue

--- src/main/java/com/back/Rq.java
+++ src/main/java/com/back/Rq.java
@@ -1,35 +1,35 @@
 package com.back;
 
 public class Rq {
+
     private String cmd;
-    public Rq(String cmd){
+
+    public Rq(String cmd) {
         this.cmd = cmd;
     }
 
-    public String getActionName(){
+
+    public String getActionName() {
 
         return cmd.split("\\?")[0];
     }
 
-    public String getParam(String inputKey,String defaultValue) {
-
-        if(cmd.equals("등록?고향=서울&이름=홍길동") && inputKey.equals("고향")){
-            return "서울";
-        }
-        if(cmd.equals("등록?고향=서울&이름=홍길동") && inputKey.equals("이름")){
-            return "홍길동";
-        }
+    public String getParam(String inputKey, String defaultValue) {
 
         String[] cmdBits = cmd.split("\\?");
+        String queryString = cmdBits[1];
+        String[] queryBits = queryString.split("&");
 
-        String[] paramBits = cmdBits[1].split("=");
-        String key = paramBits[0];
-        String value = paramBits[1];
+        for(String param : queryBits) {
+            String[] paramBits = param.split("=");
+            String key = paramBits[0];
+            String value = paramBits[1];
 
-        if(inputKey.equals(key)){
-            return value;
+            if(inputKey.equals(key)) {
+                return value;
+            }
         }
-        return defaultValue;
 
+        return defaultValue;
     }
 }
